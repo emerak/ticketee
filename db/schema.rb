8a51b0bfa2336758ae3b1c0ebec27d6e9419bc46
= <alejandra.cernas@tangosource.com>
2013-09-09 15:50:04 -0500

    Created new database schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,19 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130905224918) do
+ActiveRecord::Schema.define(version: 20130909195548) do
 
   create_table "projects", force: true do |t|
     t.string   "name"
     t.string   "description"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "users", force: true do |t|
-    t.string   "name"
-    t.string   "password"
-    t.string   "password_confirmation"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
