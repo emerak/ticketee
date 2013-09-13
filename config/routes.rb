6143cac2fef7cfcf73c708159adfc5ef2ab608a0
Alejandra Cernas <alejandra.cernas@tangosource.com>
2013-09-13 15:09:43 -0500

    Added API to created projects

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Ticketee::Application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+      resources :projects
+    end
+  end
+  
+  namespace :api do
+    namespace :v1 do
+      resources :projects
+    end
+  end
+  
   root :to => "projects#index"
   resources :users
   resources :projects do
@@ -8,12 +20,16 @@ Ticketee::Application.routes.draw do
       end
     end
   end
+  
   resources :tickets do
     resources :comments 
     resources :tags do
       member do
         delete :remove
       end 
+  
     end
+  
   end
+
 end
